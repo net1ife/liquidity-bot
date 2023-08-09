5c67eee8287a95f6ee6b8a782599e33c1f811f0c
net1ife <127271713+net1ife@users.noreply.github.com>
2023-08-09 01:24:41 -0500

    Update liquidity.py

--- liquidity.py
+++ liquidity.py
@@ -1,54 +1,64 @@
 import web3
 from web3 import Web3, exceptions
 import time
+import logging
+from logging.handlers import RotatingFileHandler
 
-w3 = Web3(Web3.HTTPProvider("https://base-mainnet.blastapi.io/18899f62-4e25-4260-aec0-d8c8ca09f056"))
+# Configuration
+NODE_URL = "https://base-mainnet.blastapi.io/18899f62-4e25-4260-aec0-d8c8ca09f056"
+MINT_EVENT_TOPIC = "0x4c209b5fc8ad50758f13e2e1088ba56a560dff690a1c6fef26394f4c03821c4f"
+LOG_FILE = "liquidity_logs.txt"
+MAX_RETRIES = 3
+SLEEP_INTERVAL = 5
 
-# Check if we're connected to the Ethereum node
-if not w3.isConnected():
-    print("Failed to connect to Ethereum node.")
-    exit()  # Exit the script if we can't connect
-else:
-    print("Connected to Ethereum node.")
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler(LOG_FILE, maxBytes=5*1024*1024, backupCount=2)  # 5 MB per file, two backup logs
+logger.addHandler(handler)
 
-# Define the topics for the Mint event
-mint_event_topic = "0x4c209b5fc8ad50758f13e2e1088ba56a560dff690a1c6fef26394f4c03821c4f"
+def initialize_web3(url):
+    w3 = Web3(Web3.HTTPProvider(url))
+    if not w3.isConnected():
+        raise ConnectionError("Failed to connect to Ethereum node.")
+    return w3
 
-# Get the latest block
-latest_block = w3.eth.block_number
+def log_liquidity_event(tx):
+    log_msg = f"Liquidity added in transaction {tx.hash.hex()} by address {tx['from']}"
+    print(log_msg)
+    logger.info(log_msg)
 
-# Define the file to store the logs
-logfile = "liquidity_logs.txt"
+def main():
+    w3 = initialize_web3(NODE_URL)
+    latest_block = w3.eth.block_number
+    retries = 0
 
-while True:
-    try:
-        new_block = w3.eth.block_number
-        if new_block != latest_block:
-            # Fetch the new block and get the transactions
-            block = w3.eth.get_block(new_block, full_transactions=True)
-            print(f"--- Checking Block #{new_block} ---")
+    while retries < MAX_RETRIES:
+        try:
+            new_block = w3.eth.block_number
+            if new_block != latest_block:
+                block = w3.eth.get_block(new_block, full_transactions=True)
+                print(f"--- Checking Block #{new_block} ---")
 
-            # Iterate through all transactions in the block
-            for tx in block.transactions:
-                # Get the receipt to access the logs
-                receipt = w3.eth.get_transaction_receipt(tx.hash)
-                # Iterate through each log
-                for log in receipt.logs:
-                    # Check if this log's topics contain the Mint event topic
-                    if mint_event_topic in log.topics:
-                        log_msg = f"Liquidity added in transaction {tx.hash.hex()} by address {tx['from']}"
-                        print(log_msg)
+                for tx in block.transactions:
+                    receipt = w3.eth.get_transaction_receipt(tx.hash)
+                    for log in receipt.logs:
+                        if MINT_EVENT_TOPIC in log.topics:
+                            log_liquidity_event(tx)
 
-                        # Also write it into the log file
-                        with open(logfile, "a") as f:
-                            f.write(log_msg + "\n")
+                latest_block = new_block
+                retries = 0  # Reset retries if block processed successfully
 
-            latest_block = new_block
+        except exceptions.BlockNotFound:
+            print(f"Block {new_block} not found. Skipping...")
+        except Exception as e:
+            print(f"Error: {e}")
+            retries += 1
+            time.sleep(SLEEP_INTERVAL)
 
-    except exceptions.BlockNotFound:
-        print(f"Block {new_block} not found. Skipping...")
-    except Exception as e:
-        print(f"Error: {e}")
-        time.sleep(5)  # Sleep a bit longer in case of an error to avoid rapid error logs
+        time.sleep(1)
 
-    time.sleep(1)
+    print("Maximum retries reached. Exiting...")
+
+if __name__ == '__main__':
+    main()
